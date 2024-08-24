dc3da666232eb85640a61dd6c747f940b0bfedfb
ashik <ashik123ks@gmail.com>
2024-08-24 11:26:17 +0530

    task set

--- taskserver/client/script.js
+++ taskserver/client/script.js
@@ -1,10 +1,42 @@
-{
-    fetch("http://localhost:3000/json").then((response)=>{
-    console.log("response",response)
 
-      response.json().then((datas) =>{
-      console.log("datas :",datas)
-    })
-    
-})
-}
+
+
+  async function fetchData() {
+
+    try {
+      
+
+    let datas = await fetch("http://localhost:3000/json");
+    let parsed_datas = await datas.json();
+    console.log("parsed_datas : ", parsed_datas);
+  
+      let rows = ''
+      for(i=0;i<parsed_datas.length;i++){
+        rows =rows + `
+
+        <div>${parsed_datas[i].title}</div>
+
+        `
+      }
+     
+
+
+  
+      let Datacontainer = document.getElementById('datacontainer');
+      Datacontainer.innerHTML=rows;
+      console.log("Datacontainer : ", Datacontainer);
+   
+
+    } catch (error) {
+      console.log("error : ", error);
+    }
+
+
+  }
+
+  fetchData();
+
+
+
+  
+
--- taskserver/server/server.js
+++ taskserver/server/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const PORT = 3000;
 const url = require('url');
 const fs = require('fs');
-const querystring = require('querystring')
 
 const server = http.createServer((req,res) =>{
 
@@ -17,37 +16,19 @@ console.log("parsed_url :", parsed_url);
 if(parsed_url.pathname === '/'){
     //serve the html file on root request
 
-    res.writeHead(200,{'Content-Type' : 'text/html'});
-    res.end(fs.readFileSync('../client/index.html'));
+    res.writeHead(200,{'Content-Type' : 'text/html'})
+    res.end(fs.readFileSync('../client/index.html'))
 }
 else if(parsed_url.pathname === '/style.css'){
-    res.writeHead(200,{'Content-Type' : 'text/css'});
-    res.end(fs.readFileSync('../client/style.css'));
+    res.writeHead(200,{'Content-Type' : 'text/css'})
+    res.end(fs.readFileSync('../client/style.css'))
 }
 else if (parsed_url.pathname === '/json'){
     res.writeHead(200,{'Content-Type' : 'text/json'})
-    res.end(fs.readFileSync('../client/datas.json'))
-}
-else if(parsed_url.pathname === '/submit' && req.method === 'POST'){
-    console.log("reached here");
-
-    let body = '';
-
-    req.on('data',(chunks)=> {
-        console.log("chunks :", chunks);
-        body += chunks.toString();
-    });
-
-    req.on('end',() => {
-        console.log("body :",body);
-        let datas =querystring.parse(body);
-        console.log("datas :",datas)
-
-        console.log("datas :",datas.name)
-        console.log("datas :",datas.email)
-        console.log("datas :",datas.password)
-
-    });
+    res.end(fs.readFileSync('../server/datas.json'))
+}else if (parsed_url.pathname === '/script.js'){
+    res.writeHead(200,{'Content-Type' : 'text/javascript'})
+    res.end(fs.readFileSync('../client/script.js'))
 }
 
 })
